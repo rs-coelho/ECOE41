874822fbf02bcd5b1d2a0b67d9224497bd9fd382
Samuel Siqueira Ferreira <samuelferrer19@gmail.com>
2020-11-22 22:18:24 -0300

    desenhando na tela

--- structures/pilha.py
+++ structures/pilha.py
@@ -1,52 +1,22 @@
 import tkinter as tk
 from functools import partial
-from structures.no import NoRedondo
+from no import NoRedondo, NoQuadrado
 
 # A pilha estão seguindo os padrões do exAddingShapes
 # Modifique para que ela aceite o NoRedondo
 
 
 class Pilha(tk.Tk):
-    shapes = ("rectangle", "oval", "arc")
 
     def __init__(self, stuff: list = (1, 2, 3)):
-        super(Pilha, self).__init__()
+
+    #     super(Pilha, self).__init__()
         self.vet = stuff
         print('Objeto Pilha com ', self.vet, ' de valor inicial')
-        self.title("Drawing standard items")
-        self.start = None
-        self.shape = None
-        self.canvas = tk.Canvas(self, bg="white")
-        frame = tk.Frame(self)
-        for shape in self.shapes:
-            btn = tk.Button(frame, text=shape.capitalize())
-            btn.config(command=partial(self.set_selection, btn, shape))
-            btn.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
-            self.canvas.bind("<Button-1>", self.draw_item)
-            self.canvas.pack()
-            frame.pack(fill=tk.BOTH)
-
-    def set_selection(self, widget, shape):
-        for w in widget.master.winfo_children():
-            w.config(relief=tk.RAISED)
-        widget.config(relief=tk.SUNKEN)
-        self.shape = shape
-
-    def draw_item(self, event):
-        x, y = event.x, event.y
-        if not self.start:
-            self.start = (x, y)
-        else:
-            x_origin, y_origin = self.start
-            self.start = None
-            bbox = (x_origin, y_origin, x, y)
-            if self.shape == "rectangle":
-                self.canvas.create_rectangle(*bbox, fill="blue", activefill="yellow")
-            elif self.shape == "oval":
-                self.canvas.create_oval(*bbox, fill="red", activefill="yellow")
-            elif self.shape == "arc":
-                self.canvas.create_arc(*bbox, fill="green", activefill="yellow")
-
+        #self.canvas = tk.Canvas(self,bg="white")
+        #self.draw = NoRedondo(160,230, self.vet[1])
+        
+        #self.draw.pack()
     def __len__(self):
         return len(self.vet)
 
@@ -55,3 +25,5 @@ class Pilha(tk.Tk):
 
     def pop(self):
         return self.vet.pop()
+app = Pilha()
+#app.mainloop()
