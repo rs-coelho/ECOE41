f7d43a9de9d848f9812cba17ea98f366cefc94f4
Rodrigo Coelho <rodrigosodrecoelho@gmail.com>
2020-11-25 22:54:44 -0300

    Pilha ok, fila view not working and no.py dreprecated

--- structures/no.py
+++ structures/no.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-
+from functools import partial
+# considerando as operações possivelmente esta classe não será utilizada
 
 class NoRedondo(tk.Tk):
     def __init__(self, x=130, y=220, r=30, v="1"):
@@ -30,28 +31,46 @@ class NoRedondo(tk.Tk):
 
 
 class NoQuadrado(tk.Tk):
-    def __init__(self, x=130, y=240, v: list = "1"):
+    def __init__(self, v: list = ["1", '2', '3']):
         super().__init__()
         self.title("Pilha")
-        self.x = x
-        self.y = y
+        self.x = 150
+        self.y = 240
         self.v = v
         self.canvas = tk.Canvas(self, bg="white")
-        # frame = tk.Frame(self)
-        self.draw_quad()
-        self.text_id = self.canvas.create_text((160, 220))
+        frame = tk.Frame(self)
         # self.canvas.bind("<Button-1>", self.draw_quad)
-        self.write_text()
+        btn = tk.Button(frame, text='pop')
+        btn.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        btn.config(command=partial(self.set_selection, btn))
+        btn2 = tk.Button(frame, text='insert')
+        btn2.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        btn2.config(command=partial(self.set_selection, btn2))
+        self.update()
         for a in range(len(self.v)):
+
+            self.canvas.bind("<Button-1>", print('Btn action'))
+            frame.pack(fill=tk.BOTH)
             self.canvas.pack()
+            self.canvas.pack()
+            self.draw_quad()
+            print('ok')
+            self.write_text(a)
 
         # self.update()
 
-    def write_text(self):
-        self.canvas.itemconfig(self.text_id, text=self.v)
+    def set_selection(self, widget):
+        for w in widget.master.winfo_children():
+            w.config(relief=tk.RAISED)
+        widget.config(relief=tk.SUNKEN)
+
+    def write_text(self, i):
+        text_id = self.canvas.create_text((self.x + 30, self.y + 20))
+        self.canvas.itemconfig(text_id, text=self.v[i])
 
     def draw_quad(self):
         box = (self.x, self.y, self.x + 60, self.y - 40)
+        self.y -= 40
         self.canvas.create_rectangle(box)
 
 
--- structures/pilha.py
+++ structures/pilha.py
@@ -1,35 +1,74 @@
 import tkinter as tk
 from functools import partial
-from structures.no import NoRedondo, NoQuadrado
 
-
-# A pilha estão seguindo os padrões do exAddingShapes
-# Modifique para que ela aceite o NoRedondo
+# Problemas de inserção
 
 
 class Pilha(tk.Tk):
 
-    def __init__(self, stuff: list = (1, 2, 3)):
+    def __init__(self, stuff: list = [1, 2, 3]):
         super(Pilha, self).__init__()
         self.vet = stuff
         print('Objeto Pilha com ', self.vet, ' de valor inicial')
-        # self.canvas = tk.Canvas(self, bg="white")
-        # self.draw = NoRedondo(160, 230, self.vet[1])
+        self.title("Pilha")
+        self.x = 150
+        self.y = 240
+        self.canvas = tk.Canvas(self, bg="white")
+        frame = tk.Frame(self)
 
-        # self.draw.pack()
+        btn = tk.Button(frame, text='insert')
+        btn.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        btn.config(command=self.append)
+        self.canvas.bind("<Button-1>", self.append(1))
+
+        btn2 = tk.Button(frame, text='pop')
+        btn2.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        btn2.config(command=self.pop)
+        self.canvas.bind("<Button-2>", self.pop())
+        #  self.update()
+        for a in range(len(self.vet)):
+            self.draw_quad()
+            self.write_text(a)
+            self.canvas.pack()
+            frame.pack(fill=tk.BOTH)
+
+    def set_selection(self, widget):
+        for w in widget.master.winfo_children():
+            w.config(relief=tk.RAISED)
+            widget.config(relief=tk.SUNKEN)
+
+    def write_text(self, i):
+        text_id = self.canvas.create_text((self.x + 30, self.y + 20))
+        self.canvas.itemconfig(text_id, text=self.vet[i])
+
+    def draw_quad(self):
+        box = (self.x, self.y, self.x + 60, self.y - 40)
+        self.y -= 40
+        self.canvas.create_rectangle(box)
 
     def __len__(self):
         return len(self.vet)
 
-    def append(self, other):
+    def append(self, other=1):
         self.vet.append(other)
+        print(self.vet)
+        self.draw_quad()
+        self.write_text(-1)
 
     def pop(self):
-        return self.vet.pop()
+        self.y += 40 * (len(self.vet) + 1)
+        self.canvas.delete(tk.ALL)
+        print(self.vet)
+        if self.vet:
+            for i in range(len(self.vet)):
+                self.draw_quad()
+                self.write_text(i)
+            return self.vet.pop()
+        else:
+            return
 
 
 if __name__ == "__main__":
     app = Pilha()
-    # app.mainloop()
+    app.mainloop()
     pass
-
