a1e20b2caa0169722acc4a60a44c924d2a433b32
Samuel Siqueira Ferreira <samuelferrer19@gmail.com>
2020-12-02 18:12:13 -0300

    funcao draw e os botoes ajustados para as lista, mas a lista n esta funcionando

--- structures/lista_duplamente_encad.py
+++ structures/lista_duplamente_encad.py
@@ -1,16 +1,46 @@
-class Node:
+import tkinter as tk
+
+class Node(tk.Tk):
     def __init__(self, item, nxt=None, prv=None):
+        super(Node, self).__init__()
         self.item = item
         self.next = nxt
         self.prev = prv
 
 
-class ListaDE:
+class ListaDE(tk.Tk):
     def __init__(self, *args: Node):
+
+        super(ListaDE, self).__init__()
+        self.title("Lista Duplamente Encadeada")
+        self.canvas = tk.Canvas(self, bg="white")
+        framelistaDE = tk.Frame(self)
+        self.x = 150
+        self.y = 220
+
+        btn = tk.Button(framelistaDE, text='insert')
+        btn.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        btn.config(command=self.insert)
+        
+        btn4 = tk.Button(framelistaDE, text='search')
+        btn4.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        btn4.config(command=self.search)
+
+        btn2 = tk.Button(framelistaDE, text='delete')
+        btn2.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        btn2.config(command=self.delete)
+
+        btn3 = tk.Button(framelistaDE, text='delete all')
+        btn3.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        btn3.config(command=self.clear)
+
+        self.canvas.pack()
+        framelistaDE.pack(fill=tk.BOTH)
+
         self.head = None
         for _ in args:
             self.insert(_)
-
+        
     def __str__(self):
         h = self.head
         r = "["
@@ -41,11 +71,12 @@ class ListaDE:
             p = p.next
         return None
 
-    def insert(self, item):
+    def insert(self, item=1):
         new_node = Node(item, self.head)
         if not self.isEmpty():
             self.head.prev = new_node
         self.head = new_node
+        print(self.head.item)
         return new_node
 
     def delete(self, val):
@@ -61,6 +92,7 @@ class ListaDE:
         if p.next:
             p.next.prev = p.prev
         del p
+        print(self.head.item)
         return True
 
     def clear(self):
@@ -73,6 +105,7 @@ class ListaDE:
             p = t
 
     def print_linked(self):
+        
         h = self.head
         r = "["
         a, b = None, None
@@ -86,8 +119,35 @@ class ListaDE:
             h = h.next
         return r + "]"
 
+    def set_selection(self, widget):
+        for w in widget.master.winfo_children():
+            w.config(relief=tk.RAISED)
+            widget.config(relief=tk.SUNKEN)
+
+    def write_text(self, i):
+        text_id = self.canvas.create_text((self.x + 30, self.y + 20))
+        self.canvas.itemconfig(text_id, text=i)
+    def draw_quad(self):
+
+
+        box = (self.x, self.y, self.x + 60, self.y - 40)
+        self.y -= 40
+        self.canvas.create_rectangle(box)
+
+        box2 = (self.x, self.y, self.x+30, self.y)
+        self.write_text("prox")
+        self.canvas.create_rectangle(box2)
+
+        box3 = (self.x, self.y, self.x-150, self.y)
+        self.write_text("ant")
+        self.canvas.create_rectangle(box3)
+
+        line = (self.x, self.y, self.x, self.y)
+        self.canvas.create_line(line,arrow=tk.LAST,fill="black",width=2)
+
 
 if __name__ == "__main__":
-    # write test code
+    app = ListaDE()
+    app.mainloop()
     pass
 
--- structures/lista_simplesmente_encad.py
+++ structures/lista_simplesmente_encad.py
@@ -1,13 +1,38 @@
-
+import tkinter as tk
 
 class Node:
-    def __init__(self, item, nxt=None):
+    def __init__(self, item = 1, nxt=None):
         self.item = item
         self.next = nxt
 
 
-class ListaSE:
+class ListaSE(tk.Tk):
     def __init__(self, *args: Node):
+
+
+        super(ListaSE, self).__init__()
+        self.x = 150
+        self.y = 220
+        self.title("Lista Simplesmente Encadeada")
+        self.canvas = tk.Canvas(self, bg="white")
+        framelistaSE = tk.Frame(self)
+
+
+        btn = tk.Button(framelistaSE, text='insert')
+        btn.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        btn.config(command=self.insert)
+
+        btn2 = tk.Button(framelistaSE, text='delete')
+        btn2.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        btn2.config(command=self.delete)
+
+        btn3 = tk.Button(framelistaSE, text='delete all')
+        btn3.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        btn3.config(command=self.clear)
+
+        self.canvas.pack()
+        framelistaSE.pack(fill=tk.BOTH)
+
         self.head = None
         for _ in args:
             self.insert(_)
@@ -44,7 +69,7 @@ class ListaSE:
             p = p.next
         return None
 
-    def insert(self, item):
+    def insert(self, item = "1"):
         new_node = Node(item, self.head)
         if not self.isEmpty():
             self.head.prev = new_node
@@ -85,7 +110,19 @@ class ListaSE:
             h = h.next
         return r + "]"
 
+    def write_text(self, i):
+        text_id = self.canvas.create_text((self.x + 30, self.y + 20))
+        self.canvas.itemconfig(text_id, text=i)
+    
+    def draw_quad(self):
+        box = (self.x, self.y, self.x + 60, self.y - 40)
+        self.y -= 40
+        self.canvas.create_rectangle(box)
+        line = (self.x, self.y, self.x+80, self.y)
+        self.canvas.create_line(line,arrow=tk.LAST,fill="black",width=2)
+        box2 = (self.x, self.y, self.x, self.y)
+        self.write_text("prox")
+        self.canvas.create_rectangle(box2)
 
 if __name__ == "__main__":
-    # write test code
     pass
