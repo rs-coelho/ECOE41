2c2355c64012b76641ca26b5a131bfb14ee62737
Samuel Siqueira Ferreira <samuelferrer19@gmail.com>
2020-12-05 22:05:59 -0300

    printando arvore bin_busca

--- structures/arvore_binaria_busca.py
+++ structures/arvore_binaria_busca.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+
 class BranchLeaf:
     def __init__(self, item, right=None, left=None):
         self.item = item
@@ -5,11 +7,44 @@ class BranchLeaf:
         self.left = left
 
 
-class BinarySearchTree:
-
+class BinarySearchTree(tk.Toplevel):
     def __init__(self, height=0):
+        
         self.head = None
         self.height = height
+        
+        
+        super(BinarySearchTree, self).__init__() 
+        self.title("Arvore de busca binaria")
+        self.canvas = tk.Canvas(self, bg = "white")
+        framearv_bus = tk.Frame(self)
+        self.x = 130
+        self.y = 30
+        self.r = 30
+        self.xr = 190
+        self.yr = 100
+          
+        self.var = tk.StringVar()
+        
+        labl = tk.Label(framearv_bus, text="Insert: ")
+        labl.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        self.entry = tk.Entry(framearv_bus, width=10, textvariable=self.var)
+        self.entry.pack(side=tk.LEFT, expand=True, fill=tk.X)
+
+        btn = tk.Button(framearv_bus, text='insert')
+        btn.pack(side=tk.LEFT, expand=True, fill=tk.X)
+        btn.config(command=self.insert)
+            
+        btn4 = tk.Button(framearv_bus, text='search')
+        btn4.pack(side=tk.LEFT, expand=True, fill=tk.X)
+        btn4.config(command=self.search)
+
+        btn2 = tk.Button(framearv_bus, text='delete')
+        btn2.pack(side=tk.LEFT, expand=True, fill=tk.X)
+        btn2.config(command=self.remove)
+        if(self.height == 0):            
+            self.canvas.pack()
+            framearv_bus.pack(fill=tk.BOTH)
 
     def __str__(self):
         return self.__print_structure()
@@ -25,23 +60,46 @@ class BinarySearchTree:
                 root = self.head.right.__print_structure(root)
         return root
 
-    def insert(self, item):
+    def insert(self):
+        
+        item = self.var.get()
+        self.entry.delete(0, 'end')
         root = self.head
         new = BranchLeaf(item)
+        
+
+        if item == '':
+            return False
 
         if root is None:
             self.head = new
+            print(" ak")
+            self.write_text(item)
+            self.draw_circle()
             self.head.left = BinarySearchTree(self.height+1)
             self.head.right = BinarySearchTree(self.height+1)
+            
 
         elif item < root.item:
-            self.head.left.insert(item)
+            
+            print("passou aki")
+            self.write_text(item)
+            self.draw_circle_left()
+            self.head.left.insert()
+            
+            
 
         elif item > root.item:
-            self.head.right.insert(item)
+            print("passou aki2")
+            self.write_text_right(item)
+            self.draw_circle_right()
+            self.head.right.insert()
+        
+        
 
     # not working properly
     def remove(self, item):
+        
         if self.head is not None:
 
             if self.head.item is item:
@@ -67,7 +125,12 @@ class BinarySearchTree:
         else:
             return False
 
-    def search(self, item):
+    def search(self):
+        item = self.var.get()
+        self.entry.delete(0, 'end')
+        if item == '':
+            return False
+        
         if self.head is not None:
 
             if self.head.item is item:
@@ -82,17 +145,65 @@ class BinarySearchTree:
 
         else:
             return
+    
+    def write_text(self, i):
+        text_id = self.canvas.create_text((self.x , self.y))
+        self.canvas.itemconfig(text_id, text=i)
+    
+    def write_text_right(self, i):
+        text_id = self.canvas.create_text((self.xr , self.yr))
+        self.canvas.itemconfig(text_id, text=i)
+
+    def draw_circle(self, x=130, y=30, r=30):
+        circle = (self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r)
+        self.canvas.create_oval(circle)
+        
+        line = ((self.x - self.r)+10, (self.y - self.r)+50, (self.x + self.r)-75, (self.y + self.r)+15)
+        self.canvas.create_line(line, arrow=tk.LAST, fill="black", width=2)
+
+        line = ((self.x - self.r)+52, (self.y - self.r)+50, (self.x + self.r)+17, (self.y + self.r)+15)
+        self.canvas.create_line(line, arrow=tk.LAST, fill="black", width=2)
+        self.x -= 70
+        self.y += 60
+    
+    def draw_circle_right(self, x=130, y=30, r=30):
+        circle = (self.xr - self.r, self.yr - self.r, self.xr + self.r, self.yr + self.r)
+        self.canvas.create_oval(circle)
+        
+        line = ((self.xr - self.r)+10, (self.yr - self.r)+50, (self.xr + self.r)-75, (self.yr + self.r)+15)
+        self.canvas.create_line(line, arrow=tk.LAST, fill="black", width=2)
+
+        line = ((self.xr - self.r)+52, (self.yr - self.r)+50, (self.xr + self.r)+17, (self.yr + self.r)+15)
+        self.canvas.create_line(line, arrow=tk.LAST, fill="black", width=2)
+
+        self.xr += 60
+        self.yr += 70
+
+    def draw_circle_left(self, x=130, y=30, r=30):
+        circle = (self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r)
+        self.canvas.create_oval(circle)
+        
+        line = ((self.x - self.r)+10, (self.y - self.r)+50, (self.x + self.r)-75, (self.y + self.r)+15)
+        self.canvas.create_line(line, arrow=tk.LAST, fill="black", width=2)
+
+        line = ((self.x - self.r)+52, (self.y - self.r)+50, (self.x + self.r)+17, (self.y + self.r)+15)
+        self.canvas.create_line(line, arrow=tk.LAST, fill="black", width=2)
+
+        self.x -= 70
+        self.y += 60
 
 
 if __name__ == "__main__":
     arvore = BinarySearchTree()
-    arvore.insert(5)
-    arvore.insert(2)
-    arvore.insert(3)
-    arvore.insert(7)
-    arvore.insert(8)
-    arvore.insert(9)
-    print(arvore.search(8))
-    print(arvore)
-    arvore.remove(7)
-    print(arvore)
+    # arvore.insert(5)
+    # arvore.insert(2)
+    # arvore.insert(3)
+    # arvore.insert(7)
+    # arvore.insert(8)
+    # arvore.insert(9)
+    # print(arvore.search(8))
+    # print(arvore)
+    # arvore.remove(7)
+    # print(arvore)
+    arvore.mainloop()
+    pass
